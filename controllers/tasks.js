749246eeb2493704a7be843becda9e8bfc1beee2
waleedhassan081 <waleedhassan081@gmail.com>
2021-10-02 21:52:11 +0400

    single task api created

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,34 @@
-const Task=require('../models/Task')
-const getAlltasks=(req,res)=>{
-    res.send('get all tasks')
+const Task = require('../models/Task')
+const getAlltasks = async (req,res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(201).json({tasks})
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    
 }
-const createTask= async (req,res)=>{
-    const task=await Task.create(req.body)
-    res.status(200).json({task})
+const createTask = async (req,res)=>{
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({task})
+    } catch (error) {
+        res.status(500).json(error)
+    }
+
 }
-const getTask=(req,res)=>{
-    res.json({id:req.params.id})
+const getTask = async (req,res) => {
+    try {
+        const {id:TaskId} = req.params
+        const task = await Task.findOne({_id:TaskId})
+        if(!task){
+            res.status(404).json({msg:`no task found on the given id ${TaskId}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({error})
+    }
+
 }
 const updateTask=(req,res)=>{
     res.send('task update')
--- models/Task.js
+++ models/Task.js
@@ -2,8 +2,15 @@ const mongoose=require('mongoose')
 
 
 const TaskSchema=new mongoose.Schema({
-    name:String,
-    completed:Boolean
+    name:{
+        type:String,
+        required:[true,'you have to provide the name'],
+        maxlength:[20,'the maximum character you can insert is 20']
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    }
 },{
     timestamps:true
 })
